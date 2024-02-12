4be6221f6c682436e54d23993913d9ed8aeb13ef
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-12 16:58:44 +0200

    how_much_i_love_you

---
+++ CodeWars/8KY/how_much_i_love_you.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+std::string how_much_i_love_you(int nb_petals)
+{
+    switch (nb_petals % 6)
+    {
+    case 1:
+        return "I love you";
+    case 2:
+        return "a little";
+    case 3:
+        return "a lot";
+    case 4:
+        return "passionately";
+    case 5:
+        return "madly";
+    case 0:
+        return "not at all";
+    default:
+        return ""; // Handle invalid input
+    }
+}
