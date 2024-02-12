e55c80d5346cb69516cb48d0e34ef2ce65970e4c
Mohamed Hema <mohamedhema2017@gmail.com>
2024-02-12 17:08:05 +0200

    another soltuon fo r reverseString

--- CodeWars/8KY/reverseString.cpp
+++ CodeWars/8KY/reverseString.cpp
@@ -6,3 +6,18 @@ string reverseString(string str)
     reverse(str.begin(), str.end());
     return str;
 }
+
+// another soluton
+
+#include <string>
+using namespace std;
+
+string reverseString(string str)
+{
+    string reversed = "";
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+        reversed += str[i];
+    }
+    return reversed;
+}
