8d8f3ed1a2ca487f8446ddcaeb7e2e838207ec2c
Mohamed <mohamedhema2017@gmail.com>
2024-03-24 23:12:15 +0200

    stack is done

--- DSA/Stack.cpp
+++ DSA/Stack.cpp
@@ -1,8 +1,71 @@
 #include <iostream>
 using namespace std;
 
+#define size 5
+int stack[size], top = -1;
+void push(int value);
+int pop();
+int peek();
+void display();
+
 int main()
 {
+    push(5);
+    push(10);
+    push(15);
+    pop();
+    display();
+    peek();
 
     return 0;
+}
+
+void push(int value)
+{
+    if (top == size - 1)
+    {
+        cout << "stackoverflow" << endl;
+    }
+    else
+    {
+        top++;
+        stack[top] = value;
+    }
+}
+int pop()
+{
+    if (top == -1)
+    {
+        cout << "stack is underflow" << endl;
+    }
+    else
+    {
+        return stack[top--];
+    }
+}
+
+int peek()
+{
+    if (top == -1)
+    {
+        cout << "stack is underflow" << endl;
+    }
+    else
+    {
+        return stack[top];
+    }
+}
+void display()
+{
+    if (top == -1)
+    {
+        cout << "stack is underflow" << endl;
+    }
+    else
+    {
+        for (int i = top; i >= 0; i--)
+        {
+            cout << stack[i] << endl;
+        }
+    }
 }
